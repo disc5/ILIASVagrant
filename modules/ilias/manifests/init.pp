ad43f8483eeb182d1fcf1925acee8f804d4f06ac
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2014-09-04 14:35:01 +0200

    Update

--- modules/ilias/manifests/init.pp
+++ modules/ilias/manifests/init.pp
@@ -19,28 +19,39 @@ class ilias {
 	     owner => 'www-data',
 	}
 
-	file {"/opt/ilias/ilias":
+	file {"/opt/ilias/shared/ilias":
 		 ensure => directory,
 		 owner => 'www-data',
 		 require     => Exec['ilias-download'],
 	}
 
-	file {'/opt/ilias/htdocs':
-	  ensure => 'link',
-	  target => '/opt/ilias/ilias',
-	}	
+	#file {'/opt/ilias/htdocs':
+	#  ensure => 'link',
+	#  target => '/opt/ilias/shared/ilias',
+	#}
+
+    file {"/opt/ilias/htdocs":
+      ensure => directory,
+      owner => 'www-data',
+    }
+
+    exec {'ilias-symlinks':
+      command => 'ln -s /opt/ilias/shared/ilias/* .',
+      cwd => '/opt/ilias/htdocs',
+      require     => [File["/opt/ilias/htdocs"], Exec['ilias-unzip']],
+      refreshonly => true,
+    }
 	
 	exec {'ilias-download':
-		command => 'wget http://sourceforge.net/projects/ilias/files/latest/download -O /opt/ilias/ilias.zip',
-		unless => 'test -f /opt/ilias/ilias.zip',
+		command => 'wget http://sourceforge.net/projects/ilias/files/latest/download -O /opt/ilias/shared/ilias.zip',
+		unless => 'test -f /opt/ilias/shared/ilias.zip',
 		path => ['/bin', '/usr/bin'],
 		notify => Exec['ilias-unzip'],
-		
 	}
 	
 	exec { 'ilias-unzip':
 	  command     => 'unzip ilias.zip',
-	  cwd         => '/opt/ilias',
+	  cwd         => '/opt/ilias/shared',
 	  path        => ['/usr/bin'],
 	  refreshonly => true,
 	  require     => Exec['ilias-download'],
